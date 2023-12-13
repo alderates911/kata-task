af3c860c06e46c3271973c63370f129090fd2e22
Islam Magomedov <sleep_paralysis@mail.ru>
2023-12-13 22:22:17 +0300

    fixed code

--- script.js
+++ script.js
@@ -26,18 +26,18 @@ const sliderWrapper = document.querySelector('.swiper-wrapper');
   
 readmore.addEventListener('click',  function func() {
   readmore.classList.toggle('repair__read-more-modified');
-  if(readmore.innerHTML === "Показать все") {
-    if(parseInt(window.getComputedStyle(document.body).width) < 1120) {
-      sliderContainer.style.height = '336px';
-      sliderWrapper.style.height = '336px';
-    }else {
-      sliderContainer.style.height = '248px';
-      sliderWrapper.style.height = '248px';
-    }
-    readmore.innerHTML = "Скрыть";
-  } else {
-    readmore.innerHTML = "Показать все";
-    sliderContainer.style.height = '160px';
-    sliderWrapper.style.height = '160px';
+  if(readmore.textContent === 'Показать все') {
+    readmore.textContent = 'Скрыть';
+  }else {
+    readmore.textContent = 'Показать все';
+  }
+
+  if(parseInt(window.getComputedStyle(document.body).width) < 1120) {
+    sliderContainer.classList.toggle('slider-container-medium');
+    sliderWrapper.classList.toggle('slider-wrapper-medium');
+  }
+  if(parseInt(window.getComputedStyle(document.body).width) > 1119) {
+    sliderContainer.classList.toggle('slider-container-large');
+    sliderWrapper.classList.toggle('slider-wrapper-large');
   }
 });
